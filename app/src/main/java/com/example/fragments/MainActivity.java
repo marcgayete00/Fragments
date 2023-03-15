2bf485f4d24040bceb6cf03ba82b262382003d5b
marcgayete00 <marc.gayete@students.salle.url.edu>
2023-03-15 11:51:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.fragments;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.menu_maps:
+                        Map mapFragment = new Map();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, mapFragment).commit();
+                        return true;
+                    case R.id.menu_weather:
+                        // Acción al seleccionar el elemento Weather
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/fragments/Map.java
@@ -0,0 +1,74 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class Map extends Fragment {
+
+    private MapView mMapView;
+    private EditText input;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflar el archivo de layout para este fragmento
+        View rootView = inflater.inflate(R.layout.menu_maps, container, false);
+
+        // Obtener la referencia al MapView
+        mMapView = rootView.findViewById(R.id.map_view);
+        input = rootView.findViewById(R.id.input);
+        String text = input.getText().toString();
+
+        // Inicializar el mapa
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                // Configurar el mapa
+                // ...
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+}
