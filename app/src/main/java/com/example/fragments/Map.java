0a1e8c8f399529d076ef1d13af43606b210d1ddd
marcgayete00 <marc.gayete@students.salle.url.edu>
2023-03-16 09:31:11 +0100

    Map almost done

--- app/src/main/java/com/example/fragments/Map.java
+++ app/src/main/java/com/example/fragments/Map.java
@@ -1,5 +1,7 @@
 package com.example.fragments;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,15 +13,38 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
 
 public class Map extends Fragment {
+
 
     private MapView mMapView;
     private EditText input;
     private Button search;
+
+    private void searchLocation(String locationName, GoogleMap googleMap) {
+        Geocoder geocoder = new Geocoder(getContext());
+        List<Address> addresses = null;
+        try {
+            addresses = geocoder.getFromLocationName(locationName, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (addresses != null && !addresses.isEmpty()) {
+            Address address = addresses.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,7 +55,6 @@ public class Map extends Fragment {
         mMapView = rootView.findViewById(R.id.map_view);
         input = rootView.findViewById(R.id.input);
         search = rootView.findViewById(R.id.search_button);
-        String text = input.getText().toString();
 
         // Inicializar el mapa
         mMapView.onCreate(savedInstanceState);
@@ -39,7 +63,24 @@ public class Map extends Fragment {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 // Configurar el mapa
-                // ...
+                LatLng location = new LatLng(40.416775, -3.703790); // Ubicación predeterminada (Madrid, España)
+                googleMap.addMarker(new MarkerOptions().position(location).title("Ubicación predeterminada"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 10)); // Zoom inicial de 10
+
+                // Agregar listener al botón "search" para ubicar la localidad ingresada en el mapa
+                search.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String text = input.getText().toString(); // Obtener el texto del EditText "input"
+
+                        // Aquí se debería implementar la lógica para obtener la ubicación de la localidad ingresada
+                        // En este ejemplo, se fija la ubicación en una localidad predefinida en Madrid
+                        LatLng location = new LatLng(40.479270, -3.614771); // Ubicación de Alcalá de Henares, Madrid
+                        googleMap.clear(); // Limpiar marcadores previos
+                        googleMap.addMarker(new MarkerOptions().position(location).title(text)); // Agregar marcador en la ubicación encontrada
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12)); // Zoom de 12
+                    }
+                });
             }
         });
 
